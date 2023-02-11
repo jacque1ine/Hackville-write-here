5222de31b26234ed15eda355d0b0a3a2c6915c45
Jacqueline Fung <fungjsl@gmail.com>
2023-02-11 14:32:12 -0500

    form working without styling and less fields

--- component/ContactForm.tsx
+++ component/ContactForm.tsx
@@ -1,42 +1,15 @@
 import React, { useRef } from "react";
 import { useEffect, useState } from "react";
 import emailjs from "@emailjs/browser";
-
-// const btn = useRef(null);
-// const btn = document.getElementById("button");
-//const [btnLoading, setbtnLoading] = useState("Send Email");
-
-//THIS IS DIFF
-// const form = useRef<HTMLFormElement>(null);
-
-// const sendEmail = (e: any) => {
-//   e.preventDefault();
-//   setbtnLoading("Loading...");
-
-//   const serviceID = "default_service";
-//   const templateID = "template_rbkpcb9";
-
-//   emailjs
-//     .sendForm(serviceID, templateID, form.current, "your public key here")
-//     .then(
-//       () => {
-//         setbtnLoading("Send Email");
-//         alert("Sent!");
-//       },
-//       (err: any) => {
-//         setbtnLoading("Send Email");
-//         alert(JSON.stringify(err));
-//       }
-//     );
-// };
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function sendEmail2(e: any) {
   e.preventDefault();
-  // console.log(form.current);
   emailjs
     .sendForm(
       "service_1128xjn",
-      "template_rbkpcb9",
+      "template_dwbvoyh",
       e.target,
       "RTKXEoUG3NUwuirJi"
     )
@@ -53,48 +26,30 @@ function sendEmail2(e: any) {
 const ContactForm = () => {
   return (
     <>
-      <div className="container">
-        <form onSubmit={sendEmail2}>
-          <div className="field">
-            <label htmlFor="subject">subject</label>
-            <input type="text" name="subject" id="subject"></input>
-          </div>
+      <form id="form" onSubmit={sendEmail2}>
+        <div className="field">
+          <label htmlFor="from_name">from_name</label>
+          <input type="text" name="from_name" id="from_name"></input>
+        </div>
+        <div className="field">
+          <label htmlFor="message">message</label>
+          <input type="text" name="message" id="message"></input>
+        </div>
+        <div className="field">
+          <label htmlFor="to_email">to_email</label>
+          <input type="text" name="to_email" id="to_email"></input>
+        </div>
+        <div className="field">
+          <label htmlFor="reply_to">reply_to</label>
+          <input type="text" name="reply_to" id="reply_to"></input>
+        </div>
 
-          <div className="field">
-            <label htmlFor="message">message</label>
-            <input type="text" name="message" id="message"></input>
-          </div>
-
-          <div className="field">
-            <label htmlFor="to_email">to_email</label>
-            <input type="text" name="to_email" id="to_email"></input>
-          </div>
-
-          <div className="field">
-            <label htmlFor="from_name">from_name</label>
-            <input
-              type="text"
-              name="from
-            _name"
-              id="from_name"
-            ></input>
-          </div>
-
-          <div className="field">
-            <label htmlFor="from_email">from_email</label>
-            <input type="text" name="from_email" id="from_email"></input>
-          </div>
-
-          <div className="field">
-            <label htmlFor="reply_to">reply_to</label>
-            <input type="text" name="reply_to" id="reply_to"></input>
-          </div>
-          {/* <input type="submit" id="button" value={"test"}></input> */}
-          <input type="submit" id="button" value={"test"}></input>
-        </form>
-      </div>
+        <input type="submit" id="button" value="Send Email"></input>
+      </form>
     </>
   );
 };
 
 export default ContactForm;
+
+//422The recipient address is empty
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,9 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="header">
-          <ContactForm></ContactForm>
-        </div>
+        <div className="header"></div>
       </main>
     </>
   );
