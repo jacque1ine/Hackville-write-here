a882361fafe5da307cb4ef73363d7e9fc97752f5
Richard Zhang <notzree@gmail.com>
2023-02-11 16:45:32 -0500

    get text from image success

--- pages/api/vision.ts
+++ pages/api/vision.ts
@@ -1,9 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import vision from '@google-cloud/vision'
+import type { NextApiRequest, NextApiResponse } from "next";
+import vision from "@google-cloud/vision";
 type Data = {
-  detections: object,
-}
+  detections: any;
+};
 
 const client = new vision.ImageAnnotatorClient();
 
@@ -11,16 +11,17 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-    try{
-    const [result] = await client.textDetection("public/detect_handwriting_OCR-detect-handwriting_SMALL.png");
-    const detections = result.textAnnotations;
-    if (detections){
-        res.status(200).json({detections: detections});
-    }
-    }
-    catch(e){
-    res.status(400);
-    }
-    
-
+  try {
+    const imgData = JSON.parse(req.body);
+    const request = {
+      image: {
+        content: Buffer.from(imgData, "base64"),
+      },
+    };
+    const [result] = await client.textDetection(request);
+    const fullTextAnnotation = result.textAnnotations;
+    res.status(200).json({ detections: fullTextAnnotation });
+  } catch (e: any) {
+    res.status(400).json({ detections: e.message });
+  }
 }
