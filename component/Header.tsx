c5a4b4a12b8bb550b26807846bc2ba3996334d7d
Annie Cai <caishajia@gmail.com>
2023-02-12 12:16:30 -0500

    Update Header.tsx

--- component/Header.tsx
+++ component/Header.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="header">
-      <img src="/logo.svg" className="header-logo"></img>
+      <div className="header"><Link href={"/"}><img src="/logo.svg" className="header-logo"></img></Link></div>
     </div>
   );
 };
