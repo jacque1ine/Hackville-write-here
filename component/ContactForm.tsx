2f8c6236327cd358c34c2cb722da60f36dd39d79
Jacqueline Fung <fungjsl@gmail.com>
2023-02-11 19:30:32 -0500

    rebased and resolved merge conflicts

--- component/ContactForm.tsx
+++ component/ContactForm.tsx
@@ -3,36 +3,31 @@ import { useEffect, useState } from "react";
 import emailjs from "@emailjs/browser";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-
-function sendEmail(e: any) {
-  e.preventDefault();
-  emailjs
-    .sendForm(
-      "service_1128xjn",
-      "template_dwbvoyh",
-      e.target,
-      "RTKXEoUG3NUwuirJi"
-    )
-    .then(
-      (result) => {
-        console.log(result.text);
-      },
-      (error) => {
-        console.log(error.text);
-      }
-    );
-}
+import { useForm, SubmitHandler } from "react-hook-form";
+type FormValues = {
+  from_name: string;
+  message: string;
+  to_email: string;
+  from_email: string;
+};
 
 const ContactForm = () => {
-  return (
-    <>
-      <Form id="form" onSubmit={sendEmail}>
+  // const { register, handleSubmit } = useForm<FormValues>();
+  // const onSubmit: SubmitHandler<FormValues> = (data: any) => console.log(data);
+  // return (
+
+  {
+    /* <Form id="form"> */
+  }
+  {
+    /* <Form id="form">
         <Form.Group className="mb-4">
           <Form.Label htmlFor="from_name">What is your name?</Form.Label>
           <Form.Control
             type="text"
             name="from_name"
             id="from_name"
+            placeholder="Enter full name"
           ></Form.Control>
         </Form.Group>
         <Form.Group className="mb-4">
@@ -50,23 +45,107 @@ const ContactForm = () => {
           ></Form.Control>
         </Form.Group>
         <Form.Group className="mb-4">
-          <Form.Label htmlFor="reply_to">What your email address</Form.Label>
+          <Form.Label htmlFor="reply_to">What your email address?</Form.Label>
           <Form.Control
             type="text"
             name="reply_to"
             id="reply_to"
           ></Form.Control>
         </Form.Group>
-        <Form.Group className="mb-4">
-          <Button type="submit" id="button" value="Send Email">
+        <Form.Group className="mb-4 d-grid gap-2">
+          <Button
+            type="submit"
+            id="button"
+            value="Send Email"
+            size="lg"
+            onSubmit={sendEmail}
+          >
             Submit
           </Button>
         </Form.Group>
+      </Form> */
+  }
+  {
+    /* ); */
+  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  function sendEmail(data: any) {
+    console.log("send email called");
+    const contactParams: any = {
+      from_name: data.from_name,
+      message: "hello there",
+      to_email: data.to_email,
+      from_email: data.from_email,
+    };
+
+    console.log("this is contact params");
+    console.log(contactParams);
+
+    emailjs
+      .send(
+        "service_1128xjn",
+        "template_dwbvoyh",
+        contactParams,
+        "RTKXEoUG3NUwuirJi"
+      )
+      // .then(
+      //   (result) => {
+      //     console.log(result.text);
+      //   },
+      //   (error) => {
+      //     console.log(error.text);
+      //   }
+      // );
+      .then(
+        function (response) {
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+  }
+  return (
+    <>
+      <Form
+        onSubmit={handleSubmit((data) => {
+          sendEmail(data);
+        })}
+        id="myForm"
+      >
+        <Form.Label>What is your name?</Form.Label>
+        <Form.Control
+          className="mb-4"
+          {...register("from_name", { required: true })}
+        />
+
+        <Form.Label>
+          What is the email address you want to send the email to?
+        </Form.Label>
+        <Form.Control
+          className="mb-4"
+          {...register("to_email", { required: true })}
+        />
+        <Form.Label>What your email address?</Form.Label>
+        <Form.Control
+          className="mb-4"
+          {...register("from_email", { required: true })}
+        />
+        <Form.Control type="submit" />
       </Form>
     </>
+
+    // const contactParams: any= {
+    //   from_name: this.from_name,
+    //   message:test,
+    //   to_email: trip.traveler.email,
+    //   from_email: trip.owner.email,
+    // };
   );
 };
 
 export default ContactForm;
-
-//422The recipient address is empty
